911f623d76f331a1f9e3b5bc56c724b3fe1bb2cf
Dana Christopherson <44937249+DanaChristopherson@users.noreply.github.com>
2020-11-26 19:48:19 -0800

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,32 @@
+let userTotal = 0;
+let compTotal = 0;
+
+
 function rollTheDice() { 
     setTimeout(function () { 
         let randomNumber1 = Math.floor(Math.random() * 6) + 1; 
         let randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+        let randomNumber3 = Math.floor(Math.random() * 6) + 1; 
+        let randomNumber4 = Math.floor(Math.random() * 6) + 1; 
 
-        document.querySelector(".userscore").innerHTML = "";
-        document.querySelector(".computerscore").innerHTML = "";
+        let userScore = randomNumber1 + randomNumber2;
+        let compScore = randomNumber3 + randomNumber4;
+
+        userTotal += userScore;
+        compTotal += compScore;
+
 
         document.querySelector(".img1").setAttribute("src", 
             "images/dice-six-face-" + randomNumber1 + ".png"); 
 
         document.querySelector(".img2").setAttribute("src", 
             "images/dice-six-face-" + randomNumber2 + ".png"); 
+
+        document.querySelector(".img3").setAttribute("src", 
+            "images/dice-six-face-" + randomNumber3 + ".png"); 
+
+        document.querySelector(".img4").setAttribute("src", 
+            "images/dice-six-face-" + randomNumber4 + ".png"); 
 
         if (randomNumber1 === randomNumber2) { 
             document.querySelector("h1").innerHTML = "Draw!"; 
@@ -24,10 +40,16 @@ function rollTheDice() {
             document.querySelector("h1").innerHTML = (" User Wins!"); 
         } 
 
-        document.querySelector(".userscore").innerHTML += randomNumber1;
-        document.querySelector(".computerscore").innerHTML += randomNumber2;
+        document.querySelector(".userScore").innerHTML = userScore;
+        document.querySelector(".compScore").innerHTML = compScore;
+        document.querySelector(".userTotal").innerHTML = userTotal;
+        document.querySelector(".compTotal").innerHTML = compTotal;
+
+        $(".newGame").show();
     }, 1000); 
+
 } 
+
 
 // function scoreboard() {
 
